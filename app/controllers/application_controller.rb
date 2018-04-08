6d9db9da1dc9e050cac94245380bd8d5de760412
bransynluther <bransynluther@gmail.com>
2018-04-08 17:00:40 +0000

    add new method for job search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def index
+  end
 
+  def search_bar
+	@search_text = BodySnatcher::WebParser.new(params[:search])
+	if @search_text.blank?
+	  redirect_to(root_path, alert("search bar is empty")) and return
+	else
+	@text = @search_text.content_text
+	@job = Job.new(text_to_process:@text)
+	while ! @job.reload.completed?
+		sleep(1)
+	end 
+	render("Results.html.erb")
+  end
+  def results
+	
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 #  root to: "application#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     mount Sidekiq::Web => '/sidekiq'
+    root to: 'home#index'
 end
